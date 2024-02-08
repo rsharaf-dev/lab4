43ec05ef4be10bc5a06b7e2bf8dee76f7deaed9e
Retaj Sharaf <rsharaf@andrew.cmu.edu>
2024-02-08 12:22:55 +0300

    please

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,8 +1,13 @@
 def convert(tmp)
-    return ((tmp-32)*5) / 9
+ 	return "Temperature must be an integer" if tmp.class != Integer
+    return "Temperature below Absolute Zero" if tmp < -474 
+    ((tmp-32)*5) / 9 
 end
 
 
 puts convert(32)          
 puts convert(50)          
 puts convert(212)
+
+puts convert("zero")
+puts convert(-500)
